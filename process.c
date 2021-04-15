56f6e034573fa15ee5ebb019a295b7268ad5055f
John O'Donnell <johnrodonnell77@gmail.com>
2021-04-15 15:49:54 -0400

    Re-added helper functions.

--- process.c
+++ process.c
@@ -11,26 +11,7 @@
 #include <MKL25Z4.h>
 #include "3140_concur.h"
 #include "shared_structs.h"
-
-// Helper Functions
-process_t* get_last (process_t* startPt) {
-	//Given a start pointer, finds the last pointer of the linked list
-	while (startPt->nextProcess != NULL) {
-		startPt = startPt->nextProcess;
-	}
-	return startPt;
-}
-
-void enable_int(uint32_t m) {
-	__set_PRIMASK(m);
-};
-
-uint32_t disable_int(void){
-	uint32_t m;
-	m = __get_PRIMASK();
-	__disable_irq();
-	return m;
-};
+#include "helper.h"
 
 
 process_t* process_queue = NULL;
